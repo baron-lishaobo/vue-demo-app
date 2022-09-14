abbfbf7bd4180e1e24d622809aacaaf7f47b5d1d
Baron <894540635@qq.com>
2022-09-14 15:54:08 +0800

    configure project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve}from'path'
+import pkg from './package.json'
 
+function pathResolve(dir:string):string {
+  console.debug('Test platform method:',process.cwd().toString())
+  return resolve(process.cwd().toString(),',',dir);
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve:{
+    alias:[
+      {
+        find:/\/@\//,
+        replacement:pathResolve('src')+'/'
+      },
+      {
+        find:/\/#\//,
+        replacement:pathResolve('types')+'/'
+      }
+    ]
+  }
 })
