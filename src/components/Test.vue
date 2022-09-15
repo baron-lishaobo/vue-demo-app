7d17781b1840e7308385cded7ab42198816191a7
Baron <894540635@qq.com>
2022-09-15 17:03:47 +0800

    update

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="container flex bg-red-500">
+    <div class="container flex flex-col bg-red-500 md:bg-pink-700 lg:bg-red-50">
         <p class="text-lg bg-red-900">
             This is Test page
+        </p>
+        <p class="red">
+            sssss
         </p>
     </div>
 </template>
